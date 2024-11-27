4c962ea94e713dac177c323582335531acc4b2fb
saya <geraldiakmal612@gmail.com>
2024-11-27 20:54:00 +0700

    aaa

--- Documents/daspro-joobsheet11/kafe10.java
+++ Documents/daspro-joobsheet11/kafe10.java
@@ -2,15 +2,17 @@ import java.util.Scanner;
 
 public class kafe10{
 
-    public static int menghitungtotalharga(int pilihanmenu,int Banyakitem){//fungsi menghitung total harga
-        int[] hargaitems = {15000,20000,2200,12000,10000,18000};
-
-        int hargatotal = hargaitems[pilihanmenu-1]*Banyakitem;
+    
+    public static double menghitungtotalharga(int pilihanmenu,int Banyakitem,double valuediskon){//fungsi menghitung total harga
+        int[] hargaitems;
+        hargaitems = new int[]{15000,20000,2200,12000,10000,18000};
+        double hargamenupilih = hargaitems[pilihanmenu];
+        double hargatotal = hargamenupilih*Banyakitem;
         return hargatotal;
     }
 
-    public static void menu(String namapelanggan,boolean isMember,String kodepromo){//fungsi printout 
-
+    public static double menu(String namapelanggan,boolean isMember,String kodepromo){//fungsi printout 
+        double diskon = 0;
         System.out.println("Halo Selamat datang, "+namapelanggan+" !");
 
         if (isMember){
@@ -19,9 +21,14 @@ public class kafe10{
 
         if (kodepromo.equals("DISKON50")){
             System.out.println("Kode Promo telah dimasukan,Selamat anda mendapatkan diskon 50%");
+            diskon = 0.5;
         }
         else if(kodepromo.equals("DISKON30")){
             System.out.println("Kode Promo telah dimasukan,Selamat anda mendapatkan diskon 30%");
+            diskon = 0.3;
+        }
+        else{
+            System.out.println("Maaf kode invalid.Tidak ada diskon buat kamu");
         }
         System.out.println("====Menu Resto Kafe====");
         System.out.println("1. Kopi Hitam - Rp 15,000");
@@ -32,19 +39,55 @@ public class kafe10{
         System.out.println("6. Mie Goreng - Rp 18,000");
         System.out.println("=========================");
         System.out.println("Silahkan pilih menu yang anda inginkan.");   
-        System.out.println("Silahkan pilih menu yang anda inginkan.");    
+        return diskon;
     }
 
 
     public static void main(String args[]){//main yang akan dilaksanakan
+        //mboh iki jeneng e opo
         Scanner mlebu10 = new Scanner(System.in);
-       menu("andre", true,"DISKON50");
-       System.out.print("\nMasukan nomor yang ingin anda pesan :");
-       int pilihanmenu = mlebu10.nextInt();
-       System.out.print("\nMasukan Jumlah item yang ingin anda pesan :");
-       int Banyakitem = mlebu10.nextInt();
+        double diskon = menu("andre", true,"null");//memanggil fungsi menu
+        int[][] pilihanmenudanbanyakitem = new int[15][2];
+        double therealtotalharga=0;
+        char memesanlagi;
+        //mulai looping utk kode memilih menu
+        for(int x=0;x<pilihanmenudanbanyakitem.length;x++){
 
-       int totalharga = menghitungtotalharga(pilihanmenu,Banyakitem);
+            System.out.print("\nMasukan nomor yang ingin anda pesan :");
+            pilihanmenudanbanyakitem[x][0]=mlebu10.nextInt();
+            pilihanmenudanbanyakitem[x][0]--;
+            System.out.print("\nMasukan Jumlah item yang ingin anda pesan :");
+            pilihanmenudanbanyakitem[x][1]=mlebu10.nextInt();
+            System.out.print("\napakah anda ingin memesan lagi?(y/n) :");
+            memesanlagi =mlebu10.next().charAt(0);
+
+            if (memesanlagi == 'y'||memesanlagi == 'Y'){
+                
+            }
+            else{
+                break;
+            }
+        }
+        //JANGAN DI UNCOMMENT!!!! hanya utk di test
+        
+        for (int a=0;a<15;a++){
+            for(int b=0;b<2;b++){
+                if(b==0){
+                System.out.println("pesanan no "+pilihanmenudanbanyakitem[a][b]);
+                }
+                else{
+                    System.out.println("dengan jumlah "+pilihanmenudanbanyakitem[a][b]); 
+                } 
+            }
+            System.out.println("Loop baris ke -"+a+" selesai"); 
+        }
+        
+        //ini spacer
+        //menghitung total harga
+        for (int a=0;a<pilihanmenudanbanyakitem.length;a++){
+        therealtotalharga += menghitungtotalharga((pilihanmenudanbanyakitem[a][0]),pilihanmenudanbanyakitem[a][1] , diskon);
+        }
+       System.out.println("Total harga untuk pesanan anda adalah Rp"+therealtotalharga);
        
     }
 }
